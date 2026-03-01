ae16324b8954f99e547578c28b1c0788ce8d1d81
Mitko Tochev <mr.sir88@gmail.com>
2026-02-28 22:01:41 -0500

    telemetry and main tap fixing

--- src/f1p/ui/components/driver/window.py
+++ src/f1p/ui/components/driver/window.py
@@ -5,7 +5,7 @@ from direct.gui.OnscreenText import OnscreenText
 from direct.showbase.DirectObject import DirectObject
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import WindowProperties, GraphicsWindow, PerspectiveLens, Camera, NodePath, TextNode, PGTop, Point3, \
-    OrthographicLens, DisplayRegion, LVecBase4f, Vec4
+    OrthographicLens, DisplayRegion, LVecBase4f, Vec4, CardMaker
 
 
 class DriverWindow(DirectObject):
@@ -115,19 +115,31 @@ class DriverWindow(DirectObject):
         return self._camera_np
 
     def make_camera_region(self) -> None:
-        dr = self.window.makeDisplayRegion(0.6875, 0.9875, 0.625, 0.925)
-        dr.setSort(100)
+        dr = self.window.makeDisplayRegion(0.6875, 0.9875, 0.6125, 0.9125)
+        dr.setSort(10)
         dr.setClearColorActive(True)
         dr.setClearColor(Vec4(0.3, 0.3, 0.3, 1))
         dr.setCamera(self.camera_np)
 
     def make_title_frame(self) -> None:
-        title_frame = DirectFrame(
+        height = 320
+        frame = DirectFrame(
             parent=self.pixel2d,
-            frameColor=(0, 0, 0, 0.5),
-            frameSize=(0, 260, 0, self.height),
-            pos=Point3(540, 0, -self.height),
+            frameColor=(0.2, 0.2, 0.2, 0.7),
+            frameSize=(0, 260, 0, height),
+            pos=Point3(540, 0, -(self.height - (self.height - height))),
+            sortOrder=0,
         )
+
+        title_frame_height = 60
+        title_frame = DirectFrame(
+            parent=frame,
+            frameColor=(0.15, 0.15, 0.15, 0.7),
+            frameSize=(0, 260, 0, title_frame_height),
+            pos=Point3(0, 0, height-title_frame_height),
+            sortOrder=10,
+        )
+        title_frame.clearColorScale()
 
         OnscreenText(
             parent=title_frame,
@@ -136,15 +148,17 @@ class DriverWindow(DirectObject):
             scale=16,
             font=self.app.text_font,
             fg=(1, 1, 1, 1),
-            pos=(10, self.height - 21, 0),
+            pos=(10, title_frame_height-21, 0),
         )
 
-        DirectFrame(
+        square = DirectFrame(
             parent=title_frame,
             frameColor=self.team_color_obj, # TODO something is not right with the color of this frame, its darker
             frameSize=(0, 19, 0, 19),
-            pos=(10, 0, self.height - 49),
+            pos=(10, 0, title_frame_height-49),
+            sortOrder=20,
         )
+        square.clearColorScale()
 
         OnscreenText(
             parent=title_frame,
@@ -153,8 +167,167 @@ class DriverWindow(DirectObject):
             scale=14,
             font=self.app.text_font,
             fg=(1, 1, 1, 1),
-            pos=(33, self.height - 44, 0),
+            pos=(33, title_frame_height-44, 0),
         )
+
+    def make_telemetry_frame(self) -> None:
+        height = 205
+        frame = DirectFrame(
+            parent=self.pixel2d,
+            frameColor=(0.2, 0.2, 0.2, 0.7),
+            frameSize=(0, 260, 0, height),
+            pos=Point3(540, 0, -(self.height - (self.height - height - 325))),
+            sortOrder=0,
+        )
+
+        title_frame_height = 30
+        title_frame = DirectFrame(
+            parent=frame,
+            frameColor=(0.15, 0.15, 0.15, 0.7),
+            frameSize=(0, 260, 0, title_frame_height),
+            pos=Point3(0, 0, height - title_frame_height),
+            sortOrder=10,
+        )
+        title_frame.clearColorScale()
+
+        OnscreenText(
+            parent=title_frame,
+            text="TELEMETRY",
+            align=TextNode.ACenter,
+            scale=16,
+            font=self.app.text_font,
+            fg=(1, 1, 1, 1),
+            pos=(260/2, title_frame_height - 21, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="SPEED",
+            align=TextNode.ALeft,
+            scale=13,
+            font=self.app.text_font,
+            fg=(0.8, 1, 0, 0.7),
+            pos=(10, height - title_frame_height - 20, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="360.00km/h",
+            align=TextNode.ALeft,
+            scale=16,
+            font=self.app.text_font,
+            fg=(1, 1, 1, 0.8),
+            pos=(10, height - title_frame_height - 40, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="223.69mph",
+            align=TextNode.ALeft,
+            scale=16,
+            font=self.app.text_font,
+            fg=(0.5, 0.5, 0.5, 1),
+            pos=(130, height - title_frame_height - 40, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="RPM",
+            align=TextNode.ALeft,
+            scale=13,
+            font=self.app.text_font,
+            fg=(0.8, 1, 0, 0.7),
+            pos=(10, height - title_frame_height - 60, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="12000",
+            align=TextNode.ALeft,
+            scale=16,
+            font=self.app.text_font,
+            fg=(1, 1, 1, 0.8),
+            pos=(10, height - title_frame_height - 80, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="GEAR",
+            align=TextNode.ALeft,
+            scale=13,
+            font=self.app.text_font,
+            fg=(0.8, 1, 0, 0.7),
+            pos=(10, height - title_frame_height - 100, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="8",
+            align=TextNode.ALeft,
+            scale=16,
+            font=self.app.text_font,
+            fg=(1, 1, 1, 0.8),
+            pos=(10, height - title_frame_height - 120, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="DRS",
+            align=TextNode.ALeft,
+            scale=13,
+            font=self.app.text_font,
+            fg=(0.8, 1, 0, 0.7),
+            pos=(10, height - title_frame_height - 140, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="ON",
+            align=TextNode.ALeft,
+            scale=16,
+            font=self.app.text_font,
+            fg=(1, 1, 1, 0.8),
+            pos=(10, height - title_frame_height - 160, 0),
+        )
+
+        OnscreenText(
+            parent=frame,
+            text="BRAKE",
+            align=TextNode.ACenter,
+            scale=13,
+            font=self.app.text_font,
+            fg=(0.8, 1, 0, 0.7),
+            pos=(130, height - title_frame_height - 60, 0),
+        )
+
+        brake_frame = DirectFrame(
+            parent=frame,
+            frameColor=(1, 1, 1, 0.7),
+            frameSize=(-10, 10, 0, 100),
+            pos=Point3(130, 0, height - title_frame_height - 165),
+            sortOrder=10,
+        )
+        brake_frame.clearColorScale()
+
+        OnscreenText(
+            parent=frame,
+            text="THROTTLE",
+            align=TextNode.ACenter,
+            scale=13,
+            font=self.app.text_font,
+            fg=(0.8, 1, 0, 0.7),
+            pos=(210, height - title_frame_height - 60, 0),
+        )
+
+        throttle_frame = DirectFrame(
+            parent=frame,
+            frameColor=(1, 1, 1, 0.7),
+            frameSize=(-10, 10, 0, 100),
+            pos=Point3(210, 0, height - title_frame_height - 165),
+            sortOrder=10,
+        )
+        throttle_frame.clearColorScale()
+
 
     def update_camera_position(self, x: Decimal, y: Decimal, z: Decimal) -> None:
         self.camera_np.setX(x + 5)
@@ -170,6 +343,7 @@ class DriverWindow(DirectObject):
         self.initialize_camera2d()
         self.make_title_frame()
         self.make_camera_region()
+        self.make_telemetry_frame()
 
     def close(self) -> None:
         self.is_open = False
